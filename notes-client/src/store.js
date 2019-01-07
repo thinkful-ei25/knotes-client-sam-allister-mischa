e1ab45bc196b48bf943866f0c6d69f5357ef00bc
mischb <mischarberlin@gmail.com>
2019-01-07 12:05:43 -0500

    ordder of import

--- notes-client/src/store.js
+++ notes-client/src/store.js
@@ -1,5 +1,7 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 // import reducers authReducer
+// import loginReducer 
+// import registrationReducer
 import thunk from 'redux-thunk';
 const store = createStore(
   combineReducers({
