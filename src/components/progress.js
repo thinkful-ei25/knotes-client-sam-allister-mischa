418b2bb476567b56aadd3f3d88bf72bf2df614f1
Sam Dalton <spd4nf@virginia.edu>
2019-01-10 11:46:18 -0600

    added progress table

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,25 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-
-import {connect} from 'react-redux';
-
-
-export function Progress(props) {
-  
-  
-  return (
-    <p>hi</p>
-  )
-  }
-const mapStateToProps = (state) => {
-  console.log(state)
-  return({
-    // loggedIn : state.auth.user ? state.auth.user.loggedIn : null
-  })
-}
-
-export default connect(mapStateToProps)(Progress);
-=======
 import {connect} from 'react-redux';
 import requiresLogin from './requires_login';
 import {fetchProgress} from '../actions/notes';
@@ -30,14 +9,51 @@ class Progress extends React.Component {
   componentDidMount(){
     this.props.dispatch(fetchProgress());
   }
+
+  generateProgress(){
+    const progressData = this.props.progress;
+    function compare(a,b){
+      if(a.note < b.note){
+        return -1;
+      }
+      if(a.note > b.note){
+        return 1;
+      }
+      return 0;
+    }
+    progressData.sort(compare);
+    const progress = progressData.map((note, index)=>{
+      return (
+        <tr key={index}>
+          <td>{note.note}</td>
+          <td>{note.mScore}</td>
+          <td>{note.correct}</td>
+          <td>{note.incorrect}</td>
+        </tr>
+      )
+    });
+    return progress;
+  }
   
   render(){
     if(!this.props.progress){
       return null;
     } else {
+      let progress = this.generateProgress();
+      
       return(
         <div>
-          {this.props.progress}
+          <table>
+            <tbody>
+              <tr>
+                <th>Note</th>
+                <th>mScore</th>
+                <th>Correct</th>
+                <th>Incorrect</th>
+              </tr>
+              {progress}
+            </tbody>
+          </table>
         </div>
       );
     }
@@ -46,12 +62,10 @@ class Progress extends React.Component {
 }
 
 const mapStateToProps = state => {
-  const {currentUser} = state.auth;
   return {
     username: state.auth.user,
-    progress: state.notes.progress
+    progress: state.note.progress
   };
 }
 
 export default requiresLogin()(connect(mapStateToProps)(Progress));
->>>>>>> 340e3e66abf8664a89fc2f03e9ca2bb7ec81e74a
