3d4be351712b6de631a734a20e175def8df803bf
mischb <mischarberlin@gmail.com>
2019-01-11 12:26:04 -0500

    added landing page css

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {Redirect, Link} from 'react-router-dom'
+import './landing-page.css';
 
 
 
@@ -8,23 +9,25 @@ import {Redirect, Link} from 'react-router-dom'
 export function LandingPage(props) {
     
     return (
-        <div>
-            <h2>Welcome to Knotes</h2>
-            <p>Knotes is an app to learn music notes. Look at the note and type the correct answer. </p>
-            <p>If the answer is correct, you will receive points. </p>
-            <p>If the answer is incorrect, you will be shown the correct answer.</p>
-            <p>After reviewing your feedback, click next to move on to the next note.</p>
-            <p>Register to begin. After registration, you will be taken to the login page</p>
-            <p><Link to="/register">Register</Link></p>
-            <p><Link to="/login">Log In</Link></p>
-            
+        <div className="landing-container">
+            <h2 className="title">Welcome to Knotes</h2>
+            <p>
+                Knotes is an app to learn music notes. 
+                Look at the note and type the correct answer.
+                If the answer is correct, you will receive points. 
+                If the answer is incorrect, you will be shown the correct answer.
+                After reviewing your feedback, click next to move on to the next note.
+                Register to begin or login now!
+            </p>
+            <div className="lp-btn-container">
+               <Link to="/register" className="btn register">Register</Link>
+               <Link to="/login" className="btn login">Log In</Link>
+            </div>
+            <div className="lp-img-container">
+               <img src="https://i.imgur.com/rMsGafK.gif" alt="dancing shark gif"/>
+            </div>
         </div>
     );
 }
 
-const mapStateToProps = state => ({
-
-    
-    });
-
-export default connect(mapStateToProps)(LandingPage);
+export default connect()(LandingPage);
